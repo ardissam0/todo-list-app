2f17a1265bd1dc1a1d694377e155b4723cf900e3
Sam Ardis <ardis.productions@gmail.com>
2020-09-18 19:23:25 -0400

    Added delete and edit functionality

--- todo_list_app/src/firebase.js
+++ todo_list_app/src/firebase.js
@@ -2,14 +2,14 @@
 import firebase from "firebase";
 
 const firebaseApp = firebase.initializeApp({
-  apiKey: "AIzaSyByybW11kAnHAU5mJTYAWyV7j98GjbvzAA",
-  authDomain: "todo-list-app-a1b10.firebaseapp.com",
-  databaseURL: "https://todo-list-app-a1b10.firebaseio.com",
-  projectId: "todo-list-app-a1b10",
-  storageBucket: "todo-list-app-a1b10.appspot.com",
-  messagingSenderId: "931999928648",
-  appId: "1:931999928648:web:a4c9b4444c7e07b033e37d",
-  measurementId: "G-SXCGFZYTLC"
+    apiKey: "AIzaSyByybW11kAnHAU5mJTYAWyV7j98GjbvzAA",
+    authDomain: "todo-list-app-a1b10.firebaseapp.com",
+    databaseURL: "https://todo-list-app-a1b10.firebaseio.com",
+    projectId: "todo-list-app-a1b10",
+    storageBucket: "todo-list-app-a1b10.appspot.com",
+    messagingSenderId: "931999928648",
+    appId: "1:931999928648:web:a4c9b4444c7e07b033e37d",
+    measurementId: "G-SXCGFZYTLC"
 });
 
 const db = firebaseApp.firestore();
