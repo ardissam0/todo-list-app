2a88c1bcd7d5e30cddfd69d31ee61013ce22a79e
Sam Ardis <ardis.productions@gmail.com>
2020-09-18 02:17:27 -0400

    added major code for todo app, working on some styling and firebase

--- todo_list_app/src/App.js
+++ todo_list_app/src/App.js
@@ -1,10 +1,48 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import { Button, FormControl, Input, InputLabel } from '@material-ui/core';
+import Todo from './Todo';
 import './App.css';
+import db from './firebase';
+import firebase from 'firebase';
 
 function App() {
+  const [todos, setTodos] = useState([]);
+  const [input, setInput] = useState('');
+
+  useEffect(() => {
+    db.collecion('todos').orderBy('timestamp', 'desc').onSnapshot(snapshot => {
+      setTodos(snapshot.docs.map(doc => doc.data().todo))
+    })
+  }, []);
+
+  const addTodo = (event) => {
+    event.preventDefault();
+
+    db.collection('todos'.add({
+      todo: input,
+      timestamp: firebase.firestore.FieldValue.serverTimestamp()
+    }));
+
+    setTodos([...todos, input]);
+    setInput('');
+  }
   return (
     <div className="App">
       <h1>Hello world</h1>
+      <form>
+        <FormControl>
+          <InputLabel>Write a Todo</InputLabel>
+          <Input value={input} onChange={event => setInput(event.target.value)} />
+        </FormControl>
+        <Button disabled={!input} type='submit' onClick={addTodo} variant="contained" color="primary">
+          Add Todo
+        </Button>
+      </form>
+      <ul>
+        {todos.map(todo => (
+          <Todo text={todo} />
+        ))}
+      </ul>
     </div>
   );
 }
---
+++ todo_list_app/src/firebase.js
@@ -0,0 +1,17 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import firebase from "firebase";
+
+const firebaseApp = firebase.intializeApp({
+        apiKey: "AIzaSyByybW11kAnHAU5mJTYAWyV7j98GjbvzAA",
+        authDomain: "todo-list-app-a1b10.firebaseapp.com",
+        databaseURL: "https://todo-list-app-a1b10.firebaseio.com",
+        projectId: "todo-list-app-a1b10",
+        storageBucket: "todo-list-app-a1b10.appspot.com",
+        messagingSenderId: "931999928648",
+        appId: "1:931999928648:web:a4c9b4444c7e07b033e37d",
+        measurementId: "G-SXCGFZYTLC"
+});
+
+const db = firebaseApp.firestore();
+
+export { db };
